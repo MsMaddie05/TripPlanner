83130af5715b25e579674c8d8e9ed6cef1027e29
MsMaddie05 <a.griesman@ufl.edu>
2026-07-20 15:54:55 -0400

    Added css styling to login page

--- TripPlanner/src/components/pages/Login.tsx
+++ TripPlanner/src/components/pages/Login.tsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import styles from './Login.module.css';
 
 interface LoginProps {
     setIsAuthenticated: Function;
@@ -9,11 +10,13 @@ const Login = ({ setIsAuthenticated } : LoginProps) => {
     const [password, setPassword] = useState("");
 
     return (
-        <div>
-            <h1>Login</h1>
-            <input type="text" id="email" name="email" placeholder="Email" onChange={(e)=> {setEmail(e.target.value)}}></input>
-            <input type="password" id="password" name="password" placeholder="Password" onChange={(e)=> {setPassword(e.target.value)}}></input>
-            <button type="submit">Submit</button>
+        <div className = {styles.pageWrapper}>
+            <div className = {styles.loginContainer}>
+                <h1 id = {styles["loginHeader"]}>Login</h1>
+                <input type="text" className = {styles.loginInputs} id= "email" name="email" placeholder="Email" onChange={(e)=> {setEmail(e.target.value)}}></input>
+                <input type="password" className = {styles.loginInputs} id= "password" name="password" placeholder="Password" onChange={(e)=> {setPassword(e.target.value)}}></input>
+                <button id = {styles["loginSubmitBtn"]}type="submit">Submit</button>
+            </div>
         </div>
     )
 }
